a747b4c77365aa215de3988e4c1aef81a1abd7a0
SydNS <sydneynsiimbe@gmail.com>
2022-01-26 20:02:13 +0300

    installed and used the pymysql to access mysql db

--- hostelbookingandroommaterecommenderapp/hostelbookingandroommaterecommenderapp/settings.py
+++ hostelbookingandroommaterecommenderapp/hostelbookingandroommaterecommenderapp/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,9 +77,17 @@ WSGI_APPLICATION = 'hostelbookingandroommaterecommenderapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_user_system',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 

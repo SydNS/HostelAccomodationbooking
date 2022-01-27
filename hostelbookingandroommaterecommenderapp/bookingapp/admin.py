451ab17d622cc373deb1f6e87b6e51c141064395
SydNS <sydneynsiimbe@gmail.com>
2022-01-27 17:07:12 +0300

    bookingapp required files

--- hostelbookingandroommaterecommenderapp/bookingapp/admin.py
+++ hostelbookingandroommaterecommenderapp/bookingapp/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Bookhosteltable
+
+
+@admin.register(Bookhosteltable)
+class BookhosteltableAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'course', 'emergency_no', 'guardian_name', 'guardian_no', 'seater', 'price',)
+    list_filter = ('uid', 'course', 'guardian_name', 'seater', 'price',)
+    search_fields = ('uid', 'course', 'emergency_no', 'guardian_name', 'guardian_no', 'seater', 'price',)
+
+# class Bookhosteltable(models.Model):
+#     uid = models.CharField(max_length=40)
+#     course = models.CharField(max_length=35)
+#     emergency_no = models.IntegerField()
+#     guardian_name = models.CharField(max_length=100)
+#     guardian_relation = models.CharField(max_length=100)
+#     guardian_no = models.CharField(max_length=20)
+#     guardian_address = models.CharField(max_length=255)
+#     duration = models.IntegerField()
+#     city = models.CharField(max_length=50)
+#     state = models.CharField(max_length=100)
+#     pincode = models.CharField(max_length=20)
+#     seater = models.IntegerField()
+#     food = models.CharField(max_length=20)
+#     room_alloted = models.IntegerField()
+#     price = models.IntegerField()
+#     booked_time = models.DateTimeField()

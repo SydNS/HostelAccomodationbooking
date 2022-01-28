45d731a0137996e060c58b7658570cb973c817a9
SydNS <sydneynsiimbe@gmail.com>
2022-01-28 04:54:23 +0300

    cleaned some of the views

--- hostelbookingandroommaterecommenderapp/bookingapp/urls.py
+++ hostelbookingandroommaterecommenderapp/bookingapp/urls.py
@@ -4,12 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    # path('', views.dashboard, name='dashboard'),
-    # path('login/', views.loginuser, name='login'),
-    # path('register/', views.Registering, name='register'),
-    # path('logout/', views.Logout, name='logout'),
-    #
-    # Auditor of auditors
+    path('', views.index, name='indexroute'),
     path('bookings/', views.bookings, name='Bookings'),
     path('make_booking/', views.makebookings, name='Make_Bookings'),
     path('edit_bookings/<int:id>/', views.editbookings, name='Edit_Bookings'),

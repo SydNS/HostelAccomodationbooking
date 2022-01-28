3e81ad773e6bd6cd472a3bab8c3729bb6dd7befd
SydNS <sydneynsiimbe@gmail.com>
2022-01-28 20:33:52 +0300

    user account urls

---
+++ hostelbookingandroommaterecommenderapp/useraccounts/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+
+    path('login/', views.loginuser, name='login'),
+    path('register/', views.Registering, name='register'),
+    path('logout/', views.Logout, name='logout'),
+
+    # path("", include("hostelbookingandroommaterecommenderapp.urls"),name="dashboard"),
+
+]

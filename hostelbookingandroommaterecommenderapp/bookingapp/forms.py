a39e67e263db90f0a2d20ee3bac02cb284b81387
SydNS <sydneynsiimbe@gmail.com>
2022-01-29 08:46:12 +0300

    rmeoved this copy of folder files

--- hostelbookingandroommaterecommenderapp/bookingapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Bookhosteltable
-
-
-class Bookhosteltableform(ModelForm):
-    class Meta:
-        model = Bookhosteltable
-        fields = "__all__"
--- hostelbookingandroommaterecommenderapp/useraccounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Useraccountsmodel
-
-
-# Register your models here.
-# @admin.register(Useraccountsmodel)
-# class USerAccountsAdmin(admin.ModelAdmin):
-#     list_filter = ()
-# admin.site.register(Useraccountsmodel)

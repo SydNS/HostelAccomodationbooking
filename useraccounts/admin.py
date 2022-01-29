1e4629e0da6ab866469b80a9d3c6041ef81f9c4c
SydNS <sydneynsiimbe@gmail.com>
2022-01-29 09:36:04 +0300

    reging the student table in the admin

--- useraccounts/admin.py
+++ useraccounts/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Useraccountsmodel
+from .models import Useraccountsmodel, Student
 
 
 # Register your models here.
-# @admin.register(Useraccountsmodel)
-# class USerAccountsAdmin(admin.ModelAdmin):
-#     list_filter = ()
-# admin.site.register(Useraccountsmodel)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name_user','gender', 'father_name', 'date_of_birth', 'fee_receipt',
+                    'address', 'city', 'state', 'pincode', 'join_year')
+    list_filter = ('name_user', 'father_name', 'date_of_birth', 'fee_receipt',
+                   'address', 'city', 'state', 'pincode', 'join_year')

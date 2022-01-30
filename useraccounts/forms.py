492d4cd0e768c67482bed3465a4603fd999a48aa
SydNS <sydneynsiimbe@gmail.com>
2022-01-30 12:59:40 +0300

    making all fields requeired

--- useraccounts/forms.py
+++ useraccounts/forms.py
@@ -30,6 +30,20 @@ class NewUserForm(UserCreationForm):
 # profile registering form
 
 class StudentForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['date_of_birth'].required = True
+        self.fields['gender'].required = True
+        self.fields['parent_name'].required = True
+        self.fields['address'].required = True
+        self.fields['city'].required = True
+        self.fields['level_of_study'].required = True
+        self.fields['phonenumber'].required = True
+        self.fields['reporting_date'].required = True
+        self.fields['studentIdnumber'].required = True
+        self.fields['state'].required = True
+
     class Meta:
         model = Student
         fields = ('name_user', 'gender', 'parent_name', 'date_of_birth',

5507904adcfec74c7d78deacce57ba51e27a3a8f
SydNS <sydneynsiimbe@gmail.com>
2022-02-01 08:29:56 +0300

    folders for static files in prodn

--- hostelbookingandroommaterecommenderapp/settings.py
+++ hostelbookingandroommaterecommenderapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'crispy_forms',
     'bookingapp',
     'useraccounts',
@@ -128,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files_folder")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),

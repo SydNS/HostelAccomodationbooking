b73f9842c68f8f6242b4585e0090c6d945de9021
SydNS <sydneynsiimbe@gmail.com>
2022-02-02 15:53:29 +0300

    new form with date widget in the meta class and cleaned

--- bookingapp/forms.py
+++ bookingapp/forms.py
@@ -1,22 +1,33 @@
+from datetime import datetime
+
 from django.forms import ModelForm
+from django import forms
+
 from .models import Bookhosteltable
 
 
 class Bookhosteltableform(ModelForm):
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['uid'].required = True
-    #     self.fields['gender'].required = True
-    #     self.fields['parent_name'].required = True
-    #     self.fields['address'].required = True
-    #     self.fields['city'].required = True
-    #     self.fields['level_of_study'].required = True
-    #     self.fields['phonenumber'].required = True
-    #     self.fields['reporting_date'].required = True
-    #     self.fields['studentIdnumber'].required = True
-    #     self.fields['state'].required = True
+    # hostel_name = models.ForeignKey('hostels.Hostel', on_delete=models.CASCADE)
+    # customer_name = models.ForeignKey('useraccounts.Student', on_delete=models.CASCADE)
+    # room_type = models.ForeignKey('roomsapp.Roommodel', on_delete=models.CASCADE)
+    # duration = models.CharField(max_length=10, choices=DURATION_SEMESTER)
+    # arrival_date = models.DateField(null=True, blank=True)
+    # payment_status = models.CharField(max_length=10, choices=PAYMENT_CHOICES)
 
     class Meta:
         model = Bookhosteltable
-        fields = "__all__"
+        fields = '__all__'
+        widgets = {
+            'arrival_date': forms.DateInput(format=('%m/%d/%Y'),
+                                             attrs={'class': 'form-control',
+                                                    'placeholder': 'Select a date',
+                                                    'type': 'date'}),
+
+        }
+
+    def clean_join_year(self):
+        arrival_date = self.cleaned_data.get('arrival_date')
+        if not (2019 < arrival_date <= datetime.now().year):
+            raise forms.ValidationError('Please enter valid Join year')
+        return arrival_date

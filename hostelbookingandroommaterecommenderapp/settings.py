b9896ade2f83a5d0c6c154dc88a339e771c6b116
SydNS <sydneynsiimbe@gmail.com>
2022-02-03 11:55:03 +0300

    new db configurations and removed the migrations

--- hostelbookingandroommaterecommenderapp/settings.py
+++ hostelbookingandroommaterecommenderapp/settings.py
@@ -98,10 +98,10 @@ WSGI_APPLICATION = 'hostelbookingandroommaterecommenderapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'db_user_system',

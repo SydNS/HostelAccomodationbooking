14d8c9806cefb312036cbf9bc7521f47daaa9645
SydNS <sydneynsiimbe@gmail.com>
2022-02-03 12:54:07 +0300

    using postgres

--- hostelbookingandroommaterecommenderapp/settings.py
+++ hostelbookingandroommaterecommenderapp/settings.py
@@ -98,10 +98,10 @@ WSGI_APPLICATION = 'hostelbookingandroommaterecommenderapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'db_user_system',
@@ -112,10 +112,10 @@ DATABASES = {
     # }
 }
 
-# import dj_database_url
-#
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
